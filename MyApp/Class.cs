6ca8ad0546d198dcf586c468b455b505813f1a15
shubham kulkarni <shubham.kulkarni@etevaconsulting.com>
2020-10-08 15:07:01 +0530

    cha

---
+++ MyApp/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+
+        
+    }
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using OpenQA.Selenium.Interactions;
+using Microsoft.VisualBasic;
+using System.Runtime.InteropServices.ComTypes;
+using Xunit;
+using OpenQA.Selenium.Remote;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace MyApp
+{
+
+    public class Program
+    {
+
+        public static IWebDriver driver = new ChromeDriver();
+        public static Actions actions => new Actions(driver);
+        public static IJavaScriptExecutor js => (IJavaScriptExecutor)driver;
+
+        //public static IAlert alert = driver.SwitchTo().Alert();
+
+        public static By TextBox => By.XPath("//input[@type='text']");
+        public static By TexList => By.XPath("//ul[@role='listbox']");
+
+
+        
+        static void Main(string[] args)
+        {
+
+            BrowserStack();
+            Console.ReadLine();
+
+        }
+        public static void BrowserStack()
+        {
+            string USERNAME = "shubhamkulkarni1";
+            string AUTOMATE_KEY = "WyWcmc2mtZc5TwQBBsFe";
+
+
+            ChromeOptions capability = new ChromeOptions();
+            capability.AddAdditionalCapability("os_version", "9.0", true);
+            capability.AddAdditionalCapability("device", "OnePlus 7", true);
+            capability.AddAdditionalCapability("real_mobile", "true", true);
+            capability.AddAdditionalCapability("browserstack.local", "false", true);
+            capability.AddAdditionalCapability("browserstack.user", USERNAME);
+            capability.AddAdditionalCapability("browserstack.key", AUTOMATE_KEY);
+           
+
+            IWebDriver driver;
+            driver = new RemoteWebDriver(new Uri("https://hub-cloud.browserstack.com/wd/hub/"), capability);
+
+
+            //Start of test case
+
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+            Console.WriteLine(driver.Title);
+
+            IWebElement query = driver.FindElement(By.Name("q"));
+            query.SendKeys("selenium");
+            query.Submit();
+            Console.WriteLine(driver.Title);
+
+            driver.Quit();
+        }
+        public static string getConfig(string parameter)
+        {
+            string configStream = File.ReadAllText(@"../../../Practise.json");
+            var config = JObject.Parse(configStream.ToString());
+            string configValue = config.GetValue(parameter).ToString();
+            return configValue;
+        }
+    }
+}
